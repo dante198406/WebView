aba76261d785463b06faa1f91e8db567454422fd
dante198406 <dante198406@gmail.com>
2017-02-09 23:33:32 +0800

    更新为第二版

--- app/src/main/java/com/example/kel/test/MainActivity.java
+++ app/src/main/java/com/example/kel/test/MainActivity.java
@@ -2,39 +2,53 @@ package com.example.kel.test;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
-//import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
     private WebView mWebView;
     private ProgressDialog mProgressDialog;
+    private CustomProgressDialog mCustomProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(mToolbar);
 
         mWebView = (WebView) findViewById(R.id.webview);
-        mProgressDialog = new ProgressDialog(this);
-        //mProgressDialog.setTitle("123");
-        mProgressDialog.setMessage("Loading...");
+        //mProgressDialog = new ProgressDialog(this);
+        //mProgressDialog.setMessage("Loading...");
+        //mCustomProgressDialog = new CustomProgressDialog(this, "", R.drawable.frame);
+        mCustomProgressDialog = new CustomProgressDialog(this, "", R.drawable.frame, R.style.dialog);
+
         mWebView.getSettings().setJavaScriptEnabled(true);
+        //mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//net:ERR_CACHE_MISS, but seems not work
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setAllowFileAccess(true);
         webSettings.setBuiltInZoomControls(true);
         //webView.setWebViewClient(new WebViewClient());
         mWebView.setWebViewClient(new WebViewClient() {
-
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 mWebView.loadUrl(url);
@@ -44,17 +58,18 @@ public class MainActivity extends Activity {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                mProgressDialog.show();
+                //mProgressDialog.show();
+                //mCustomProgressDialog.show();
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                mProgressDialog.hide();
+                //mProgressDialog.hide();
+                //mCustomProgressDialog.hide();
             }
         });
         mWebView.loadUrl("http://www.mbizzone.com/mobile");
-
     }
 
     @Override
